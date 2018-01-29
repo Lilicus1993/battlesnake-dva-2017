ba6672b6b824122cff73f1d40354660824b9711d
SynthesizedSoul <SynthesizedSoul@users.noreply.github.com>
2018-01-28 23:26:38 -0800

    Cleanup game module.

--- server/views.py
+++ server/views.py
@@ -5,16 +5,14 @@ from rest_framework.decorators import parser_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from server.helpers.sanitizer import sanitize_start_data, sanitize_move_data
-from game.game_manager import GameManager
-
-GAME_MANAGER = GameManager()
+import game.game as bs_game
 
 @api_view(['POST'])
 @parser_classes((JSONParser,))
 def start(request):
     """Handles a start request"""
     data = sanitize_start_data(request.data)
-    game = GAME_MANAGER.create_or_get_game(data)
+    game = bs_game.factory(data)
     dva = game.dva
 
     response = dict(
@@ -31,7 +29,7 @@ def start(request):
 def move(request):
     """Handles a move request"""
     data = sanitize_move_data(request.data)
-    game = GAME_MANAGER.create_or_get_game(data)
+    game = bs_game.factory(data)
     game.update(data)
     dva = game.dva
 
